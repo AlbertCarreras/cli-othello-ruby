70efa8efa9c97e66b81845f0969c3a987490b0e7
mattgellert <mattgellert@gmail.com>
2017-09-24 19:00:12 -0400

    1-player cpu can formulate change_array

--- app/models/cpu.rb
+++ app/models/cpu.rb
@@ -39,24 +39,24 @@ class CPU
     x = 0
     y = 0
     t = 0
-    final_change_array = {}
+    final_change_hash = {}
     board_string = board.to_s
-    test_board = eval(board_string)
+    # test_board = eval(board_string)
 
     while x < 8
       while y < 8
+        test_board = eval(board_string)
         move = Move.new(x_coor: x, y_coor: y)
         change_array = move.check_move(self)
         if change_array.compact.length > 0
-          binding.pry
-          # final_change_array << change_array
+          # final_change_hash << change_array
           change_array.compact.flatten(1).each do |chips|
             test_board.send(:[]=, chips[0], chips[1], self.current_chip)
           end
 
           # get total score
           count = self.get_total_score(test_board)
-          final_change_array["test_board_#{t}".to_sym] = {
+          final_change_hash["test_board_#{t}".to_sym] = {
             chips_to_change: change_array,
             count: count
           }
@@ -69,15 +69,35 @@ class CPU
     end
 
     # return move that is most advantagous
+    max = -100
+    spread = ""
+    final_chips_to_change = []
+    final_change_hash.each do |test_board, data|
+      if self.current_chip == 0
+        spread = data[:count][:zero] - data[:count][:one]
+      else
+        spread = data[:count][:one] - data[:count][:zero]
+      end
 
-    
-    binding.pry
+      if spread > max
+        max = spread
+        final_chips_to_change = data[:chips_to_change].compact.flatten(1)
+      end
+    end
+
+    if final_chips_to_change.length > 0
+      final_chips_to_change.each do |chips|
+        board.send(:[]=, chips[0], chips[1], self.current_chip)
+      end
+    end
+    board
   end
 
   def make_move
     x = 3
     y = 5
-    final_change_array = []
+    t = 0
+    final_change_hash = {}
     board_string = Board.last.matrix_string
     test_board = eval(board_string)
 
@@ -87,27 +107,43 @@ class CPU
           move = Move.new(x_coor: x, y_coor: y)
           change_array = move.check_move(self).compact.flatten(1)
           if change_array.length > 0
-            final_change_array << change_array
             change_array.each do |chips|
               test_board.send(:[]=, chips[0], chips[1], self.current_chip)
             end
+            # guess human response
+            test_board = self.greatest_lead_response(test_board, (self.current_chip - 1).abs, "human")
+            # respond with highest separation of chips
+            test_board = self.greatest_lead_response(test_board, self.current_chip, "cpu")
+              # take total score
+              count = self.get_total_score(test_board)
+              # enter score into final_change_hash
+              final_change_hash["test_#{t}".to_sym] = {
+                change_array: change_array,
+                lead: count[:one] - count[:zero]
+              }
+
+            t += 1
           end
-
-
-        # guess human response
-        test_board = self.greatest_lead_response(test_board, (self.current_chip - 1).abs, "human")
-
-        # respond with highest separation of chips
-        test_board = self.greatest_lead_response(test_board, self.current_chip, "cpu")
-          # take total score
 
         y += 1
       end
+      y = 0
       x += 1
     end
 
     # choose original viable man with most favorable total score
+    max = -100
+    spread = ""
+    final_chips_to_change = []
+    final_change_hash.each do |test_board, data|
+      spread = data[:lead]
 
+      if spread > max
+        max = spread
+        final_chips_to_change = data[:change_array]
+      end
+    end
+    binding.pry
 
   end
 
